1cf76146f527b937c46cdea821d20fad0bd1a52e
Anastasia <nastya.bokun@gmail.com>
2020-01-14 00:08:51 +0300

    fixes 1 - login, registration

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,108 +3,69 @@ import {Link} from "react-router-dom";
 
 function LoginInput({login, onChangeLogin}) {
     return (
-        <p>
+        <div>
             <label> Login:
                 <input type="text" name="login" className="log-form-input" value={login} onChange={({target: {value}}) => {
                     onChangeLogin(value);
                 }}/>
             </label>
-        </p>
+        </div>
     );
 }
 
 function PasswordInput({password, onChangePassword}) {
     return (
-        <p>
+        <div>
             <label> Password:
                 <input type="password" name="password" className="log-form-input" value={password} onChange={({target: {value}}) => {
                     onChangePassword(value);
                 }}/>
             </label>
-        </p>
+        </div>
     );
 }
 
 function NameInput({name, onChangeName}) {
     return (
-        <p><label> User Name:
-            <input type="text" name="name" className="log-form-input" value={name} onChange={({target: {value}}) => {
-                onChangeName(value);
-            }}/>
-        </label>
-        </p>
+        <div>
+            <label> User Name:
+                <input type="text" name="name" className="log-form-input" value={name} onChange={({target: {value}}) => {
+                    onChangeName(value);
+                }}/>
+            </label>
+        </div>
     );
 }
 
 function SubmitButton(props){
     return (
-        <a><Link to='/articles' className="submit-btn">Create</Link></a>
+        <Link to='/articles' className="submit-btn">Create</Link>
     );
 }
 
 function SwitchModeButton (props) {
     return (
-        <a><Link to='/login' className="switch-mode-btn">Already have an account?</Link></a>
+        <Link to='/login' className="switch-mode-btn">Already have an account?</Link>
     );
 }
 
 class RegistrationForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: {
-                login: '',
-                password: '',
-                name: ''
-            }
-        };
-    }
+        state = {
+            login: '',
+            password: '',
+            name: ''
+        }
 
-    onChangePassword = (password) =>{
-        this.setState(prevState => {
-            let user = Object.assign({}, prevState.user);
-            user.password = password;
-            return {user};
-        });
-    };
-
-    onChangeName = (name) => {
-        this.setState(prevState => {
-            let user = Object.assign({}, prevState.user);
-            user.name = name;
-            return {user};
-        });
-    };
-
-    onChangeLogin = (login) => {
-        this.setState(prevState => {
-            let user = Object.assign({}, prevState.user);
-            user.login = login;
-            return {user};
-        });
-    };
-
-    // onSubmit = (event) => {
-    //     event.preventDefault();
-    //     console.log(`${this.state.login}, добро пожаловать!`);
-    //     const {user: {login, password}} = this.state;
-    //     const res = fetch('fuck.you/api/login', {
-    //         method: 'POST',
-    //         body: {
-    //             login,
-    //             password,
-    //         }
-    //     }).then(res => res.json()).catch(err => console.error(err));
-    // };
+    onChange = key => value => this.setState({[key]: value})
 
 
     render() {
-        const user = this.state.user;
+        const {login, password, name} = this.state;
         return (
             <form className="log-form">
-                <LoginInput login={user.login} onChangeLogin={this.onChangeLogin}/>
-                <PasswordInput password={user.password} onChangePassword={this.onChangePassword}/>
-                <NameInput name={user.name} onChangeName={this.onChangeName}/>
+                <LoginInput login={login} onChangeLogin={this.onChange('login')}/>
+                <PasswordInput password={password} onChangePassword={this.onChange('password')}/>
+                <NameInput name={name} onChangeName={this.onChange('name')}/>
                 <SubmitButton/>
                 <SwitchModeButton/>
             </form>
